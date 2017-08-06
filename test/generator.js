268248a62fec69136b148f083b95712e5c6cc858
Matthew Berryman <matthew@acrossthecloud.net>
2017-08-06 18:58:20 +1000

    get random seeds from SQS FIFO queue

--- test/generator.js
+++ test/generator.js
@@ -3,7 +3,7 @@ var expect = require("chai").expect,
 
 describe("Text generator", function() {
   it("generates a string", function() {
-    var generated_string = generator.generate();
+    var generated_string = generator.generate(null);
     expect(typeof(generated_string)==='string' && generated_string.length > 0);
   });
 });
