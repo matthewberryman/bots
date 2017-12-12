59205c710c420c43e1bd794eb0f653c04d699425
Matthew Berryman <matthew@acrossthecloud.net>
2017-12-12 14:38:59 +1100

    syntax bug fix

--- libdm/security.js
+++ libdm/security.js
@@ -13,7 +13,7 @@ security.get_challenge_response = function(crc_token, consumer_secret) {
   hmac = crypto.createHmac('sha256', consumer_secret).update(crc_token).digest('base64');
 
   return hmac;
-}
+};
 
 
 module.exports = security;
