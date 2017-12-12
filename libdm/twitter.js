116337c0e8aa98c7ed65768a7a73575173e64e1f
Matthew Berryman <matthew@acrossthecloud.net>
2017-12-12 13:50:17 +1100

    twitter env variable reference fix

--- libdm/twitter.js
+++ libdm/twitter.js
@@ -5,7 +5,7 @@ var twitter = {};
 twitter.oauth = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  token: process.env.TWITTER_ACCESS_TOKEN,
+  token: process.env.TWITTER_ACCESS_TOKEN_KEY,
   token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
