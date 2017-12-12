2f7eded1e5eb59c99b0c02cf8485cac73d8432a5
Matthew Berryman <matthew@acrossthecloud.net>
2017-12-13 10:20:41 +1100

    Use newer node image for docker builds

--- docker-npm.js
+++ docker-npm.js
@@ -12,7 +12,7 @@ Usage:
 
 const childProcess = require('child_process');
 
-const nodejsImage = 'node:6.10';
+const nodejsImage = 'node:8';
 const innerWorkingDir = '/src';
 const dockerArgs = [
   'run', '-i',
