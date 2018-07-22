f846376173f226ada694e93c881b890b5d64492b
Matthew Berryman <matthew@acrossthecloud.net>
2018-07-23 08:19:55 +1000

    villages, closes #10

--- generator.js
+++ generator.js
@@ -7,13 +7,15 @@ module.exports.generate = (seed) => {
 
   var rands = [Math.floor( mt.random() * plot_elements.murdered_person.length),
                Math.floor( mt.random() * plot_elements.cause_of_death.length),
+               Math.floor( mt.random() * plot_elements.village),
                Math.floor( mt.random() * plot_elements.village_group.length),
                Math.floor( mt.random() * plot_elements.angry_at.length),
                Math.floor( mt.random() * plot_elements.threatened.length)];
 
   return plot_elements.murdered_person[rands[0]] +
          " is found " + plot_elements.cause_of_death[rands[1]] +
-         ". Suspicion falls on the village " + plot_elements.village_group[rands[2]] +
-         ", angry that " + plot_elements.angry_at[rands[3]] +
-         " might threaten " + plot_elements.threatened[rands[4]]+".";
+         ". Suspicion falls on " + plot_elements.village[rands[2]]
+         "’s"+ plot_elements.village_group[rands[3]] +
+         ", angry that " + plot_elements.angry_at[rands[4]] +
+         " might threaten " + plot_elements.threatened[rands[5]]+".";
 };
